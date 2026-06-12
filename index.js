159ac0a1689732ce97f5cd28bb9530c1253e970e
Devansh678 <mickysingh678@gmail.com>
2026-06-12 08:35:52 +0000

    Added same changes like split,reverse and join

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 let name = "jarry";
-let age = 34;
-console.log(name);
-console.log(age);
+// let age = 34;
+// console.log(name);
+// console.log(age);
+// let splitName= name.split("");
+// console.log(splitName)
+// splitName.reverse();
+// console.log(splitName);
+// let rev = splitName.join();
+// console.log(rev);
+let rname = name.split("").reverse().join()
+console.log(rname);
